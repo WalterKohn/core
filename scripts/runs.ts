eb9ea5dfa10e65810fc843e0f8aae3d0c11196d8
WalterKohn <WalterKohn@protonmail.com>
2023-03-28 14:24:17 +0800

    add basic ops

--- scripts/runs.ts
+++ scripts/runs.ts
@@ -1,28 +1,72 @@
 import {ethers} from "hardhat";
-import {LensHub__factory} from "../typechain-types";
-import {CreateProfileDataStruct} from "../typechain-types/LensHub";
-import {ZERO_ADDRESS} from "../tasks/helpers/utils";
+import {LensHub__factory, ProfileCreationProxy__factory} from "../typechain-types";
+import {CreateProfileDataStruct, PostDataStruct} from "../typechain-types/LensHub";
+import {waitForTx, ZERO_ADDRESS} from "../tasks/helpers/utils";
+import {defaultAbiCoder} from "ethers/lib/utils";
 
 // https://docs.lens.xyz/docs/deployed-contract-addresses
 const addrs = require("./deployments/mumbai_deployments.json")
 
 async function main() {
 
-    const user = (await ethers.getSigners())[0];
+    const user0 = (await ethers.getSigners())[0];
+    const user1 = (await ethers.getSigners())[1];
 
-    const lensHub = LensHub__factory.connect(addrs['LensHubProxy'], user);
+    const profileProxy = ProfileCreationProxy__factory.connect(addrs['MockProfileCreationProxy'], user0);
+    const lensHub = LensHub__factory.connect(addrs['LensHubProxy'], user0);
+    const freeCollectModule = LensHub__factory.connect(addrs['FreeCollectModule'], user0);
 
-    const inputStruct: CreateProfileDataStruct = {
-        to: user.address,
-        handle: 'k03271',
-        imageURI: 'https://ipfs.io/ipfs/QmY9dUwYu67puaWBMxRKW98LPbXCznPwHUbhX5NeWnCJbX',
-        followModule: ZERO_ADDRESS,
-        followModuleInitData: [],
-        followNFTURI: 'https://ipfs.io/ipfs/QmTFLSXdEQ6qsSzaXaCSNtiv6wA56qq87ytXJ182dXDQJS',
-    };
+    // ========================= Create Profile ================================
+    // const inputStruct: CreateProfileDataStruct = {
+    //     to: user.address,
+    //     handle: 'sd0328',
+    //     imageURI: 'https://ipfs.io/ipfs/QmY9dUwYu67puaWBMxRKW98LPbXCznPwHUbhX5NeWnCJbX',
+    //     followModule: ZERO_ADDRESS,
+    //     followModuleInitData: [],
+    //     followNFTURI: 'https://ipfs.io/ipfs/QmTFLSXdEQ6qsSzaXaCSNtiv6wA56qq87ytXJ182dXDQJS',
+    // };
+    // let tx = await profileProxy.proxyCreateProfile(inputStruct);
+    // console.log("tx:", tx.hash);
+
+    // ========================= Post ================================
+    // const inputStruct: PostDataStruct = {
+    //     profileId: 29757,
+    //     contentURI: 'https://ipfs.io/ipfs/Qmby8QocUU2sPZL46rZeMctAuF5nrCc7eR1PPkooCztWPz',
+    //     collectModule: freeCollectModule.address,
+    //     collectModuleInitData: defaultAbiCoder.encode(['bool'], [false]), // followerOnly
+    //     referenceModule: ZERO_ADDRESS,
+    //     referenceModuleInitData: [],
+    // };
+    // await lensHub.post(inputStruct);
+
+    // ========================= Collect ================================
+    // await lensHub.collect(29757, 1, []); // self collect
+    // await lensHub.connect(user1).follow([29757], [[]]); // follow once
+    // await lensHub.connect(user1).collect(29757, 2, []); // collect by others
+
+    // Follow & Collect can be done without Holding Profiles
+
+    // ========================= Mirror ================================
+    // const inputStruct: CreateProfileDataStruct = { 
+    //     to: user1.address,
+    //     handle: 'sd.user1',
+    //     imageURI: 'https://ipfs.io/ipfs/QmY9dUwYu67puaWBMxRKW98LPbXCznPwHUbhX5NeWnCJbX',
+    //     followModule: ZERO_ADDRESS,
+    //     followModuleInitData: [],
+    //     followNFTURI: 'https://ipfs.io/ipfs/QmTFLSXdEQ6qsSzaXaCSNtiv6wA56qq87ytXJ182dXDQJS',
+    // };
+    // await profileProxy.connect(user1).proxyCreateProfile(inputStruct);  // 29762
     
-    let tx = await lensHub.connect(user).createProfile(inputStruct);
-    console.log("tx:", tx.hash);
+    const mirrorStruct = {
+        profileId: 29762,
+        profileIdPointed: 29757,
+        pubIdPointed: 1,
+        referenceModuleData: [],
+        referenceModule: ZERO_ADDRESS,
+        referenceModuleInitData: []
+    }
+    await lensHub.connect(user1).mirror(mirrorStruct);
+
 }
 
 main().catch((error) => {
---
+++ scripts/view.ts
@@ -0,0 +1,76 @@
+import {ethers} from "hardhat";
+import {CollectNFT__factory, LensHub__factory} from "../typechain-types";
+
+const addrs = require("./deployments/mumbai_deployments.json")
+
+async function main() {
+
+    const user0 = (await ethers.getSigners())[0];
+
+    const lensHub = LensHub__factory.connect(addrs['LensHubProxy'], user0);
+
+    // ========================= Profile ID ================================
+    // console.log(`Total supply: ${await lensHub.totalSupply()}`);
+    // console.log(`Profile by owner: ${await lensHub.tokenOfOwnerByIndex(user.address, 0)}`); // 29757
+    // console.log(`Profile ID by handle: ${await lensHub.getProfileIdByHandle("sd0328.test")}`);
+
+    // ========================= Profile Data ================================
+    // let profile = await lensHub.getProfile(29757);
+    // let profile = await lensHub.getProfile(29762);
+    // console.log(`pubCount: ${profile.pubCount}`);
+    // console.log(`followModule: ${profile.followModule}`);
+    // console.log(`followNFT: ${profile.followNFT}`);
+    // console.log(`handle: ${profile.handle}`);
+    // console.log(`imageURI: ${profile.imageURI}`);
+    // console.log(`followNFTURI: ${profile.followNFTURI}`);
+    
+    // pubCount: 2
+    // followModule: 0x0000000000000000000000000000000000000000
+    // followNFT: 0x1Bc396aC9b3Dd3D3c73e301B9a54cB6E71c1e206
+    // handle: sd0328.test
+    // imageURI: https://ipfs.io/ipfs/QmY9dUwYu67puaWBMxRKW98LPbXCznPwHUbhX5NeWnCJbX
+    // followNFTURI: https://ipfs.io/ipfs/QmTFLSXdEQ6qsSzaXaCSNtiv6wA56qq87ytXJ182dXDQJS
+
+    // ========================= Pub Data ================================
+    // let pub = await lensHub.getPub(29757, 1);
+    let pub = await lensHub.getPub(29762, 1);
+    console.log(`profileIdPointed: ${pub.profileIdPointed}`); // original profile id
+    console.log(`pubIdPointed: ${pub.pubIdPointed}`); // original publication id
+    console.log(`contentURI: ${pub.contentURI}`);
+    console.log(`referenceModule: ${pub.referenceModule}`);
+    console.log(`collectModule: ${pub.collectModule}`);
+    console.log(`collectNFT: ${pub.collectNFT}`);
+    
+    // profileIdPointed: 0
+    // pubIdPointed: 0
+    // contentURI: https://ipfs.io/ipfs/Qmby8QocUU2sPZL46rZeMctAuF5nrCc7eR1PPkooCztWPz
+    // referenceModule: 0x0000000000000000000000000000000000000000
+    // collectModule: 0x0BE6bD7092ee83D44a6eC1D949626FeE48caB30c
+    // collectNFT: 0x0000000000000000000000000000000000000000
+
+    // ========================= Collect NFT ================================
+    // const collectNFTAddr = await lensHub.getCollectNFT(29757, 1);
+    // const collectNFT = CollectNFT__factory.connect(collectNFTAddr, user0);
+    //
+    // const publicationContentURI = await lensHub.getContentURI(29757, 1);
+    // const totalSupply = await collectNFT.totalSupply();
+    // const ownerOf = await collectNFT.ownerOf(1);
+    // const collectNFTURI = await collectNFT.tokenURI(1);
+    // const ownerOf2 = await collectNFT.ownerOf(2);
+    //
+    // console.log(`Collect NFT total supply: ${totalSupply}`);
+    // console.log(`Collect NFT owner of ID 1: ${ownerOf}`);
+    // console.log(`Collect NFT owner of ID 2: ${ownerOf2}`);
+    // console.log(`Collect NFT URI: ${collectNFTURI}, publication content URI (should be the same): ${publicationContentURI}`);
+    
+    // Collect NFT total supply: 2
+    // Collect NFT owner of ID 1: 0x1fD0A4b25D83461fF6963a718aE631C96453DF55
+    // Collect NFT owner of ID 2: 0x1A4dCDb6DcB984a337a30937507548F68A8e734B
+    // Collect NFT URI: https://ipfs.io/ipfs/Qmby8QocUU2sPZL46rZeMctAuF5nrCc7eR1PPkooCztWPz, 
+    // publication content URI (should be the same): https://ipfs.io/ipfs/Qmby8QocUU2sPZL46rZeMctAuF5nrCc7eR1PPkooCztWPz
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
